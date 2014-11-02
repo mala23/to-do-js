a0943fb2ebe3a8ea2b35e9ac201a5ca782720a3f
mala23 <mail@mala23.me>
2014-11-02 13:22:44 +0000

    write out steps

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,2 +1,37 @@
 //Problem: User interaction doesn't provide desired results.
 //Solution: Add behaviour so the user can manage daily task.
+
+//Add a new task
+  //When the user clicks on the button
+  //Create a new list item with the text from #new-task:
+    //input (checkbox)
+    //label
+    //input (text)
+    //button.edit
+    //button.delete
+    //Each elements, needs modified and appended
+
+//Edit an existing task
+  //When the user clicks on the Edit button
+    //if the class of the parent is .editMode
+      //Switch from editMode
+      //label text become the input's value
+    //else
+      //Switch to editMode
+      //input value becomes the label's text
+    //Toggle .editMode on the parent
+
+
+//Delete an existing task
+  //When the user clicks on the Delete button
+    //Remove the parent list item from the url
+
+
+//Mark a task as complete
+  //When the Checkbox is checked
+    //Append the task list item to the #completed-tasks
+
+
+//Mark a task as incomplete
+  //When the checkbox is unchecked
+    //Append the task list item to the #incomplete-tasks
