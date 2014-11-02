7f7782c30f875ac23550c0e7991223ad8cb36ace
mala23 <mail@mala23.me>
2014-11-02 13:28:43 +0000

    create functions

--- public/js/interface.js
+++ public/js/interface.js
@@ -2,6 +2,7 @@
 //Solution: Add behaviour so the user can manage daily task.
 
 //Add a new task
+var addTask = function() {
   //When the user clicks on the button
   //Create a new list item with the text from #new-task:
     //input (checkbox)
@@ -10,8 +11,10 @@
     //button.edit
     //button.delete
     //Each elements, needs modified and appended
+};
 
 //Edit an existing task
+var editTask = function() {
   //When the user clicks on the Edit button
     //if the class of the parent is .editMode
       //Switch from editMode
@@ -20,18 +23,22 @@
       //Switch to editMode
       //input value becomes the label's text
     //Toggle .editMode on the parent
-
+};
 
 //Delete an existing task
+var deleteTask = function() {
   //When the user clicks on the Delete button
     //Remove the parent list item from the url
-
+};
 
 //Mark a task as complete
+var taskCompleted = function() {
   //When the Checkbox is checked
     //Append the task list item to the #completed-tasks
-
+};
 
 //Mark a task as incomplete
+var taskIncomplete = function() {
   //When the checkbox is unchecked
     //Append the task list item to the #incomplete-tasks
+};
