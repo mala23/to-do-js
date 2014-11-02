737c6547dea63784b98a3ceb7fb3df77df13a236
mala23 <mail@mala23.me>
2014-11-02 18:02:03 +0000

    create event handlers

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,13 +1,14 @@
 //Problem: User interaction doesn't provide desired results.
 //Solution: Add behaviour so the user can manage daily task.
 
-var taskInput = document.getElementByID('new-task'); // new-task
-var addButton; // first button
-var incompleteTasksHolder = document.getElementByID('incomplete-tasks'); // incomplete-tasks
-var completedTasksHolder = document.getElementByID('completed-tasks'); // completed-tasks
+var taskInput = document.getElementById('new-task'); // new-task
+var addButton = document.getElementsByTagName('button')[0]; // first button
+var incompleteTasksHolder = document.getElementById('incomplete-tasks'); // incomplete-tasks
+var completedTasksHolder = document.getElementById('completed-tasks'); // completed-tasks
 
 //Add a new task
 var addTask = function() {
+  console.log('Add task...');
   //When the user clicks on the button
   //Create a new list item with the text from #new-task:
     //input (checkbox)
@@ -20,6 +21,7 @@ var addTask = function() {
 
 //Edit an existing task
 var editTask = function() {
+  console.log('Edit task...');
   //When the user clicks on the Edit button
     //if the class of the parent is .editMode
       //Switch from editMode
@@ -32,18 +34,24 @@ var editTask = function() {
 
 //Delete an existing task
 var deleteTask = function() {
+  console.log('Delete task...');
   //When the user clicks on the Delete button
     //Remove the parent list item from the url
 };
 
 //Mark a task as complete
 var taskCompleted = function() {
+  console.log('Complete task...');
   //When the Checkbox is checked
     //Append the task list item to the #completed-tasks
 };
 
 //Mark a task as incomplete
 var taskIncomplete = function() {
+  conosle.log('Task incomplete...');
   //When the checkbox is unchecked
     //Append the task list item to the #incomplete-tasks
 };
+
+//Set the click handler to the addTask function
+addButton.onclick = addTask;
