a088be65b524756c5945c146496f7b3f255aeabe
mala23 <mail@mala23.me>
2014-11-02 22:44:50 +0000

    user can edit items

--- public/js/interface.js
+++ public/js/interface.js
@@ -56,14 +56,26 @@ var addTask = function() {
 //Edit an existing task
 var editTask = function() {
   console.log('Edit task...');
-  //When the user clicks on the Edit button
+
+  var listItem = this.parentNode;
+
+  var editInput = listItem.querySelector('input[type=text]');
+  var label = listItem.querySelector('label');
+
+  var containsClass = listItem.classList.contains('editMode');
+
     //if the class of the parent is .editMode
+    if(containsClass) {
       //Switch from editMode
       //label text become the input's value
-    //else
+      label.innerText = editInput.value;
+    } else {
       //Switch to editMode
       //input value becomes the label's text
+      editInput.value = label.innerText;
+    }
     //Toggle .editMode on the parent
+    listItem.classList.toggle('editMode');
 };
 
 //Delete an existing task
